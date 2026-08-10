993f9351dea559564395a433cab806c4f0cf0bbf
b4d <b4d@sablun.org>
2026-08-10 10:26:47 +0200

    Initial Commit

---
+++ NotificationCounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotificationCounter
+//
+//  Created by Deni Bacic on 10. 8. 2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotificationCounter/NotificationCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationCounterApp.swift
+//  NotificationCounter
+//
+//  Created by Deni Bacic on 10. 8. 2026.
+//
+
+import SwiftUI
+
+@main
+struct NotificationCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
